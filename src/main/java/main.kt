08814259250ee391a300e1a778833da34e2b474d
INGENIERITO8090 <127001506+INGENIERITO8090@users.noreply.github.com>
2023-07-18 11:57:49 -0500

    Add files via upload

---
+++ src/main/java/main.kt
@@ -0,0 +1,107 @@
+import kotlin.math.PI
+import kotlin.math.pow
+/*
+fun main(){
+
+//calcular el area de un triangulo
+   /* println("Calcular área del triángulo")
+
+    println("Ingresa la base: ")
+
+    val base: Int = readLine()!!.toInt()
+
+    println("Ingresa la altura: ")
+
+    val height: Int = readLine()!!.toInt()
+
+    var area = (base * height) / 2
+
+    println("El área es: $area")*/
+
+
+    //  Ingresar dos números por teclado y sumarlos.
+
+              /*  println("sumas dos numeros")
+                println(" ingresa el numero 1: ")
+                val n1:Int = readLine()!!.toInt()
+                println("ingrese el Numero 2: ")
+                val n2:Int = readLine()!!.toInt()
+                val sum=n1+n2
+              println(" la suma de los dos  numeros el igual a $sum")*/
+
+
+  //  Ingresar un número y visualizar el número elevado al cuadrado
+
+     /* println("el cuadrado de un numero ")
+      println("digite el numero que va a elevar a l cuadrado :")
+
+      val n1:Int = readLine()!!.toInt()
+      val r=n1*n1
+      println("$n1 elevado a el cuadrado es $r")*/
+
+// . Escribir un algoritmo que convierta de euros a dólares
+     /*
+     println("conversor de moneda")
+     val dol : Double = 1.12
+    println("digite la cantidad de euros : ")
+     val eu: Int = readLine()!!.toInt()
+    val vl=eu*dol
+    println(" $eu en dolares es $vl")
+    */
+
+//  Escribir un algoritmo que pida el lado de un cuadrado y muestre el valor del área y del
+//perímetro
+    /*
+  println("calcula el area de de u cuadrado y el perimetro en base a un lado ")
+    println("digite el lado de el cadruadrado de cm : ")
+     val l:Int= readLine()!!.toInt()
+     val area= l*l
+     val perimeter=l*4
+    println("el area de el cuadrado es $area \n el perimetro es $perimeter ")*/
+
+   // Escribir un algoritmo que determine el área y el volúmen de un cilindro.
+   // v π r² h
+   // a  2π r h + 2π r²
+/*
+
+    println(" algoritmo que calcula el area y el volumen de un cilindro segun su altura y radio ")
+    println("digite la altura de el  cilindro en cm  : ")
+    val h: Double = readLine()!!.toDouble()
+    println("digite el  radio de el  cilindro en cm  : ")
+    val rad:Double= readLine()!!.toDouble()
+    val volume = PI*(rad).pow(2)*h
+    val area =  2*(PI*rad).pow(2)+ (PI*2*rad*h)
+   println("el area de el  cilindro es $area \n el volumen de el cilindro es $volume")*/
+
+
+    // . Realizar un algoritmo que lea el radio de una circunferencia y escriba la longitud de la misma y
+    //el área (pi*r)^2 del círculo inscrito.
+
+/*    println("calcular el la longitud y area de una circuferencia")
+    println("Digite el radio de la circuferencia cm³ ")
+    val r:Double= readLine()!!.toDouble()
+    val  area= PI*(r).pow(2)
+    val length= 2* PI*r
+    println("el area de la circuferencia es  $area cm³  \n la longitud de la circuferencia es $length cm³")*/
+
+    // Calcular el promedio de tres (3) números ingresados por teclado
+    /*  println("Calcular el promedio de 3 numeros ")
+
+    println("digite el 1er numero : ")
+    val n1:Int= readLine()!!.toInt()
+    println("digite el 2do el numero : ")
+    val n2:Int= readLine()!!.toInt()
+    println("digite el 3er numero : ")
+    val n3:Int= readLine()!!.toInt()
+
+    val sum=n1+n2+n3
+    val prom=sum/3
+    println("el promedio de los numeros es $prom") */
+
+
+}*/
+fun main(){
+
+    
+}
+
